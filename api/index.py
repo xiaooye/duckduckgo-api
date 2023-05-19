86dc77193d4e55cad70403ff8213691fbe20a7ce
xiaooye <wxin9@gatech.edu>
2023-05-19 08:37:16 +0000

    FIX

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
-from duckduckgo_search import ddgs
+from duckduckgo_search import DDGS
 app = Flask(__name__)
-
+ddgs = DDGS()
 
 @app.route('/search')
 def search():  # put application's code here
