7a2089b42054c896eb5497aeb09741adb1f1fb38
xiaooye <wxin9@gatech.edu>
2023-05-19 08:45:22 +0000

    fine

--- api/index.py
+++ api/index.py
@@ -1,13 +1,12 @@
 from flask import Flask, request
-from duckduckgo_search import DDGS
+from duckduckgo_search import ddg
 app = Flask(__name__)
-ddgs = DDGS()
 
 @app.route('/search')
 def search():  # put application's code here
     keywords = request.args.get('q')
     max_results = int(request.args.get('max_results') or "3")
-    results = ddgs.text(keywords, region='wt-wt', timelimit='m', safesearch='Off')
+    results = ddg(keywords, region='wt-wt', max_results=max_results,time='m', safesearch='Off')
     return results
 
 
